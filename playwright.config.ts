f42d5cd9d502dfd752de82ad22c5a769ca4b3beb
aokiken <aokiken@me.com>
2022-09-20 18:57:37 +0900

    Update playwright.config.ts
    trace: 'on'
    video: 'on'
    screenshot: 'on'

--- playwright.config.ts
+++ playwright.config.ts
@@ -39,7 +39,9 @@ const config: PlaywrightTestConfig = {
     // baseURL: 'http://localhost:3000',
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
-    trace: 'on-first-retry',
+    trace: 'on',
+    video: 'on',
+    screenshot: 'on',
   },
 
   /* Configure projects for major browsers */
